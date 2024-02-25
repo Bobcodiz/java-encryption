082daaf687ebfe5feaf8c42a61f79188f561ee4d
Bobcodiz <mirowe.okinyi22@students.dkut.ac.ke>
2024-02-25 16:05:46 +0300

    ...

---
+++ java_encryption/src/main/java/com/codiz/java_encryption/cryptography/SymmetricKey.java
@@ -0,0 +1,4 @@
+package com.codiz.java_encryption.cryptography;
+
+public class SymmetricKey {
+}
--- java_encryption/src/main/java/com/codiz/java_encryption/dto/ManagedDto.java
+++ java_encryption/src/main/java/com/codiz/java_encryption/dto/ManagedDto.java
@@ -1,4 +1,9 @@
 package com.codiz.java_encryption.dto;
 
+import lombok.Data;
+
+@Data
 public class ManagedDto {
+    private String username;
+    private String password;
 }
