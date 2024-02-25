eb1e768fb3773fdc5b9fc5996ac1eac6dfe3ba32
Bobcodiz <mirowe.okinyi22@students.dkut.ac.ke>
2024-02-25 15:54:42 +0300

    ...

---
+++ java_encryption/src/main/java/com/codiz/java_encryption/JavaEncryptionApplication.java
@@ -0,0 +1,13 @@
+package com.codiz.java_encryption;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaEncryptionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaEncryptionApplication.class, args);
+	}
+
+}
