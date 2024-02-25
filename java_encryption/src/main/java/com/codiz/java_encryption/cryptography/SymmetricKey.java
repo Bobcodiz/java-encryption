c3e6729d0c8ebf697c04e537a75b894a81b28bc7
Bobcodiz <mirowe.okinyi22@students.dkut.ac.ke>
2024-02-25 16:12:35 +0300

    ...

--- java_encryption/src/main/java/com/codiz/java_encryption/cryptography/SymmetricKey.java
+++ java_encryption/src/main/java/com/codiz/java_encryption/cryptography/SymmetricKey.java
@@ -1,4 +1,23 @@
 package com.codiz.java_encryption.cryptography;
 
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import java.security.SecureRandom;
+
 public class SymmetricKey {
+
+    private static final String AES = "AES";
+
+    public static SecretKey secretAESKey() throws Exception{
+        //creating an instance of secure random
+        SecureRandom secureRandom = new SecureRandom();
+
+        // passing the string to the key generator
+        KeyGenerator keyGenerator = KeyGenerator.getInstance(AES);
+
+        //initializing the key generator to 256 bit
+        keyGenerator.init(256,secureRandom);
+        SecretKey key = keyGenerator.generateKey();
+        return key;
+    }
 }
