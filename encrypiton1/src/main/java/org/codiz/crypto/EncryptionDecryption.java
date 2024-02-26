c885ee7aa20da1577a41926b6de9cb1d30c53217
Bobcodiz <mirowe.okinyi22@students.dkut.ac.ke>
2024-02-26 13:31:27 +0300

    working on AES

--- encrypiton1/src/main/java/org/codiz/crypto/EncryptionDecryption.java
+++ encrypiton1/src/main/java/org/codiz/crypto/EncryptionDecryption.java
@@ -1,6 +1,8 @@
 package org.codiz.crypto;
 
 import javax.crypto.*;
+import javax.crypto.spec.GCMParameterSpec;
+import java.security.InvalidAlgorithmParameterException;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 import java.util.Base64;
@@ -8,6 +10,8 @@ import java.util.Base64;
 public class EncryptionDecryption {
     private SecretKey key;
     private int Key_Size;
+    private int tLen;
+    Cipher encryptionCipher;
 
     public void init() throws NoSuchAlgorithmException {
         KeyGenerator generator = KeyGenerator.getInstance("AES");
@@ -17,13 +21,26 @@ public class EncryptionDecryption {
 
     public String encrypt(String message) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
         byte[] messageToEncrypt = message.getBytes();
-        Cipher encryptionCipher = Cipher.getInstance("AES/GCM/NoPadding");
+        encryptionCipher = Cipher.getInstance("AES/GCM/NoPadding");
         encryptionCipher.init(Cipher.ENCRYPT_MODE,key);
         byte[] encryptedMessage = encryptionCipher.doFinal(messageToEncrypt);
         return encode(encryptedMessage);
     }
+    public String decrypt(String encryptedMessage) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidAlgorithmParameterException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
+        byte[] message = decode(encryptedMessage);
+
+        Cipher decryptionCipher = Cipher.getInstance("AES/GCM/NoPadding");
+        GCMParameterSpec spec = new GCMParameterSpec(tLen,encryptionCipher.getIV());
+        decryptionCipher.init(Cipher.DECRYPT_MODE,key,spec);
+        byte[] decryptedMessage = decryptionCipher.doFinal(message);
+        return new String(decryptedMessage);
+
+    }
 
     public String encode(byte[] data){
         return Base64.getEncoder().encodeToString(data);
+    }
+    public byte[] decode(String data){
+        return Base64.getDecoder().decode(data);
     }
 }
