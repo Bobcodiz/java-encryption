658923ca249f1dd6989d5b8a28649ee8eec1e6d9
Bobcodiz <mirowe.okinyi22@students.dkut.ac.ke>
2024-02-26 13:22:52 +0300

    ...

--- encrypiton1/src/main/java/org/codiz/crypto/EncryptionDecryption.java
+++ encrypiton1/src/main/java/org/codiz/crypto/EncryptionDecryption.java
@@ -1,8 +1,9 @@
 package org.codiz.crypto;
 
-import javax.crypto.KeyGenerator;
-import javax.crypto.SecretKey;
+import javax.crypto.*;
+import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
 
 public class EncryptionDecryption {
     private SecretKey key;
@@ -12,5 +13,17 @@ public class EncryptionDecryption {
         KeyGenerator generator = KeyGenerator.getInstance("AES");
         generator.init(Key_Size);
         key = generator.generateKey();
+    }
+
+    public String encrypt(String message) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
+        byte[] messageToEncrypt = message.getBytes();
+        Cipher encryptionCipher = Cipher.getInstance("AES/GCM/NoPadding");
+        encryptionCipher.init(Cipher.ENCRYPT_MODE,key);
+        byte[] encryptedMessage = encryptionCipher.doFinal(messageToEncrypt);
+        return encode(encryptedMessage);
+    }
+
+    public String encode(byte[] data){
+        return Base64.getEncoder().encodeToString(data);
     }
 }
