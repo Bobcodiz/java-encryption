1006e3fe299bab098b1fb893698413db893df8dc
Bobcodiz <mirowe.okinyi22@students.dkut.ac.ke>
2024-02-26 13:15:53 +0300

    ...

---
+++ encrypiton1/src/main/java/org/codiz/crypto/EncryptionDecryption.java
@@ -0,0 +1,12 @@
+package org.codiz.crypto;
+
+import javax.crypto.SecretKey;
+
+public class EncryptionDecryption {
+    private SecretKey key;
+    private int Key_Size;
+
+    public void init(){
+
+    }
+}
