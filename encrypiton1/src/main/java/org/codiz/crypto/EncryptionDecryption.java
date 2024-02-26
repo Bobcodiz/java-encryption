4e223079626032b7a0d49f9256380aa00b7c7770
Bobcodiz <mirowe.okinyi22@students.dkut.ac.ke>
2024-02-26 13:17:45 +0300

    ...

--- encrypiton1/src/main/java/org/codiz/crypto/EncryptionDecryption.java
+++ encrypiton1/src/main/java/org/codiz/crypto/EncryptionDecryption.java
@@ -1,12 +1,16 @@
 package org.codiz.crypto;
 
+import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
+import java.security.NoSuchAlgorithmException;
 
 public class EncryptionDecryption {
     private SecretKey key;
     private int Key_Size;
 
-    public void init(){
-
+    public void init() throws NoSuchAlgorithmException {
+        KeyGenerator generator = KeyGenerator.getInstance("AES");
+        generator.init(Key_Size);
+        key = generator.generateKey();
     }
 }
