6f0f3ee5f3b276a08cd8bc5bada9fe5bf0b4f20b
Bobcodiz <mirowe.okinyi22@students.dkut.ac.ke>
2024-02-26 10:04:37 +0300

    ...

---
+++ AES/src/main/java/org/codiz/encrypt/encryption.java
@@ -0,0 +1,11 @@
+package org.codiz.encrypt;
+
+public class encryption {
+
+    // variable declaration
+    private static final String SECRET_KEY = "mirowebob";
+    private static final String SALT_VALUE = "dadanKimathi";
+
+    //create an encryption method
+
+}
--- java_encryption/src/main/java/com/codiz/java_encryption/cryptography/SymmetricKey.java
+++ java_encryption/src/main/java/com/codiz/java_encryption/cryptography/SymmetricKey.java
@@ -1,18 +1,22 @@
 package com.codiz.java_encryption.cryptography;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
 import java.security.SecureRandom;
+import java.util.Base64;
+
 @Component
+@Slf4j
 public class SymmetricKey {
 
     private static final String AES = "AES";
 
     public static SecretKey secretAESKey() throws Exception{
         //creating an instance of secure random
-        SecureRandom secureRandom = new SecureRandom();
+        SecureRandom secureRandom = SecureRandom.getInstanceStrong();
 
         // passing the string to the key generator
         KeyGenerator keyGenerator = KeyGenerator.getInstance(AES);
@@ -20,6 +24,7 @@ public class SymmetricKey {
         //initializing the key generator to 256 bit
         keyGenerator.init(256,secureRandom);
         SecretKey key = keyGenerator.generateKey();
+        log.info("The secret key is: ");
         return key;
     }
 }
