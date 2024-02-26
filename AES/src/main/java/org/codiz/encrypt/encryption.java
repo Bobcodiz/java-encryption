d7f05a57aef7228aee6483b474e88838dcb457c7
Bobcodiz <mirowe.okinyi22@students.dkut.ac.ke>
2024-02-26 10:30:19 +0300

    ...

--- AES/src/main/java/org/codiz/encrypt/encryption.java
+++ AES/src/main/java/org/codiz/encrypt/encryption.java
@@ -1,7 +1,13 @@
 package org.codiz.encrypt;
 
+import javax.crypto.Cipher;
+import javax.crypto.SecretKey;
 import javax.crypto.SecretKeyFactory;
-import java.security.KeyFactory;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.PBEKeySpec;
+import javax.crypto.spec.SecretKeySpec;
+import java.nio.charset.StandardCharsets;
+import java.security.spec.KeySpec;
 
 public class encryption {
 
@@ -14,9 +20,15 @@ public class encryption {
         try {
             //initialize the byte array
             byte[] initVector = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+            IvParameterSpec ivParameterSpec = new IvParameterSpec(initVector);
             //create factory for keys
             SecretKeyFactory keyFactory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA256");
             /*PBEKeySpec class implements KeySpec interface */
+            KeySpec keySpec = new PBEKeySpec(SECRET_KEY.toCharArray(),SALT_VALUE.getBytes(StandardCharsets.UTF_8),25536,256);
+            SecretKey secret = keyFactory.generateSecret(keySpec);
+            SecretKeySpec secretKeySpec = new SecretKeySpec(secret.getEncoded(),"AES");
+            Cipher cipher = Cipher.getInstance("AES/CBC/PKS5Padding");
+            cipher.init(Cipher.ENCRYPT_MODE,secretKeySpec,ivParameterSpec);
         }catch (Exception e){
 
         }
