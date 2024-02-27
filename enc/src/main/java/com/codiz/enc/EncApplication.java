bb798209a6f2b5e9d9f9852fc739b5667733ecb0
Bobcodiz <mirowe.okinyi22@students.dkut.ac.ke>
2024-02-27 15:51:08 +0300

    ...

---
+++ enc/src/main/java/com/codiz/enc/EncApplication.java
@@ -0,0 +1,13 @@
+package com.codiz.enc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EncApplication.class, args);
+	}
+
+}
---
+++ encryption/src/main/java/com/codiz/encryption/service/controller/ManagerControll.java
@@ -0,0 +1,4 @@
+package com.codiz.encryption.service.controller;
+
+public class ManagerControll {
+}
