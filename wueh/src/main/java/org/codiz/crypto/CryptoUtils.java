269785838552b075ca22343a403407a2749510b1
Bobcodiz <mirowe.okinyi22@students.dkut.ac.ke>
2024-02-29 08:21:38 +0300

    ...

--- wueh/src/main/java/org/codiz/crypto/CryptoUtils.java
+++ wueh/src/main/java/org/codiz/crypto/CryptoUtils.java
@@ -1,10 +1,16 @@
 package org.codiz.crypto;
 
-import javax.crypto.KeyGenerator;
+import javax.crypto.*;
+import javax.crypto.spec.GCMParameterSpec;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
 
 public class CryptoUtils {
-
+    private SecretKey SECRET_KEY;
+    private byte[] IV;
+    private int T_Len;
     public void init(){
         try {
             KeyGenerator generator = KeyGenerator.getInstance("AES");
@@ -13,5 +19,40 @@ public class CryptoUtils {
         } catch (NoSuchAlgorithmException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    public String encode(String message){
+        try {
+            byte[] messageToEncode = message.getBytes();
+            Cipher encryptionCipher = Cipher.getInstance("AES/GCM/NoPadding");
+            encryptionCipher.init(Cipher.ENCRYPT_MODE,SECRET_KEY);
+            IV = encryptionCipher.getIV();
+            byte[] encryptedBytes = encryptionCipher.doFinal(messageToEncode);
+            return encode(encryptedBytes);
+
+        } catch (NoSuchPaddingException | InvalidKeyException | NoSuchAlgorithmException | IllegalBlockSizeException |
+                 BadPaddingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    public String decrypt(String encryptedMessage){
+        try {
+            byte[] messageToDecrypt = decode(encryptedMessage);
+            Cipher decryptionCipher = Cipher.getInstance("AES/GCM/NoPadding");
+            GCMParameterSpec spec = new GCMParameterSpec(T_Len,IV);
+            decryptionCipher.init(Cipher.DECRYPT_MODE,SECRET_KEY,spec);
+            byte[] decryptedBytes = decryptionCipher.doFinal(messageToDecrypt);
+            return new String(decryptedBytes);
+        } catch (NoSuchPaddingException | NoSuchAlgorithmException | InvalidAlgorithmParameterException |
+                 InvalidKeyException | IllegalBlockSizeException | BadPaddingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public String encode(byte[] data){
+        return Base64.getEncoder().encodeToString(data);
+    }
+    public byte[] decode(String data){
+        return Base64.getDecoder().decode(data);
     }
 }
